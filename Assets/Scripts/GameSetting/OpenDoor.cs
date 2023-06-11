3f83bbb27801f0aa1d5fa58aa4622931df34abe4
jungizz <songjy0118@gmail.com>
2023-06-12 01:27:21 +0900

    edit comment

--- Assets/Scripts/GameSetting/OpenDoor.cs
+++ Assets/Scripts/GameSetting/OpenDoor.cs
@@ -6,9 +6,9 @@ using UnityEngine.Tilemaps;
 public class OpenDoor : MonoBehaviour
 {
     public GameObject Enemy;
-    public GameObject Key; //Ű �̹��� UI
+    public GameObject Key; //키 이미지 UI
 
-    //����, ���� �� Ÿ��
+    //닫힌, 열린 문 타일
     public Tilemap tilemap;
     public TileBase closeDoorTile1;
     public TileBase closeDoorTile2;
@@ -23,8 +23,8 @@ public class OpenDoor : MonoBehaviour
     private bool openByKey = false;
 
 
-    //ī�޶� �̵� ���� ����
-    public float moveDirection; //���� 1, ���� -1
+    //카메라 이동 관련 변수
+    public float moveDirection; //수평 1, 수직 -1
     private GameObject mainCamera;
     private Vector3 destination;
     private bool enterDoor = false;
@@ -46,7 +46,7 @@ public class OpenDoor : MonoBehaviour
 
     private void Update()
     {
-        //�� ������ ���� �� �׾��� �� ���� ������ ���
+        //각 던전에 적이 다 죽었을 때 문이 열리는 경우
         if (openByEnemy &&  Enemy.transform.childCount < 1) 
         {
             Open();
@@ -62,7 +62,7 @@ public class OpenDoor : MonoBehaviour
 
     private void FixedUpdate()
     {
-        //ī�޶� �̵�
+        //카메라 이동
         if (enterDoor)
         {
             mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, destination, 0.1f);
@@ -70,15 +70,15 @@ public class OpenDoor : MonoBehaviour
         if (mainCamera.transform.position == destination) enterDoor = false;
     }
 
-    //�� ����
+    //문 열기
     public void Open()
     {
         doorSound.Play();
 
-        //�ݶ��̴��� Trigger�� Ȱ��ȭ�ؼ� ��� �����ϵ���
+        //콜라이더의 Trigger를 활성화해서 통과 가능하도록
         GetComponent<BoxCollider2D>().isTrigger = true;
 
-        //���� �� Ÿ���� ���� �� Ÿ�Ϸ� �ٲٱ�
+        //닫힌 문 타일을 열린 문 타일로 바꾸기
         tilemap.SwapTile(closeDoorTile1, openDoorTile1);
         tilemap.SwapTile(closeDoorTile2, openDoorTile2);
         tilemap.SwapTile(closeDoorTile3, openDoorTile3);
@@ -89,7 +89,7 @@ public class OpenDoor : MonoBehaviour
     {
         if (collision.CompareTag("Player"))
         {
-            //Ű�� ������ ���� ���� �� ����
+            //키를 가지고 있을 때만 문 열기
             if (openByKey && Key.activeSelf)
             {
                 Open();
@@ -97,22 +97,22 @@ public class OpenDoor : MonoBehaviour
                 openByKey = false;
             }
 
-            if (moveDirection == 1) //�������θ� �̵� ������ ��
+            if (moveDirection == 1) //수평으로만 이동 가능한 문
             {
-                //ī�޶� ������ ���ʿ� ������ 1, �ƴϸ� -1
+                //카메라가 문보다 왼쪽에 있으면 1, 아니면 -1
                 if (mainCamera.transform.position.x < transform.GetComponent<BoxCollider2D>().bounds.center.x) playerDirX = 1; 
                 else playerDirX = -1;
 
-                //ī�޶��� ������ ����
+                //카메라의 목적지 설정
                 destination = new Vector3(mainCamera.transform.position.x + 17.7f * playerDirX, mainCamera.transform.position.y, mainCamera.transform.position.z);
             }
-            else if (moveDirection == -1) //�������θ� �̵� ������ ��
+            else if (moveDirection == -1) //수직으로만 이동 가능한 문
             {
-                //ī�޶� ������ �Ʒ��� ������ 1, �ƴϸ� -1
+                //카메라가 문보다 아래에 있으면 1, 아니면 -1
                 if (mainCamera.transform.position.y < transform.GetComponent<BoxCollider2D>().bounds.center.y) playerDirY = 1;
                 else playerDirY = -1;
 
-                //ī�޶��� ������ ����
+                //카메라의 목적지 설정
                 destination = new Vector3(mainCamera.transform.position.x, mainCamera.transform.position.y + 9.0f * playerDirY, mainCamera.transform.position.z);
             }
             enterDoor = true;
